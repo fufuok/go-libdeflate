88d1eeb090bcb7853dc868ca8cae625f9d39dc25
Fufu <fufuok@qq.com>
2025-10-21 15:20:06 +0800

    chore: add examples

---
+++ examples/pool/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/fufuok/bytespool"
+
+	"github.com/fufuok/go-libdeflate"
+)
+
+func init() {
+	// Optional: Set to true to minimize the capacity of returned slices, but adds one copy operation
+	// libdeflate.SetReduceMemoryUsage(true)
+
+	// Optional: Set the initial and maximum size of the byte pool (default 2B ~ 8MiB)
+	// libdeflate.InitDefaultPools(32, 128<<20)
+}
+
+func main() {
+	smallDataDemo()
+	poolDemo()
+}
+
+func smallDataDemo() {
+	c, _ := libdeflate.NewCompressorAutoClose()
+	n, _, err := c.Compress(nil, nil, libdeflate.ModeZlib)
+	fmt.Println(n, err) // 0 libdeflate: native: empty input
+
+	dst, err := Zip(nil)
+	fmt.Println(len(dst), err) // 0 <nil>
+
+	data := []byte("f")
+	n, _, err = c.Compress(data, nil, libdeflate.ModeZlib)
+	fmt.Println(n, err) // 12 <nil>
+
+	out, err := Zip(data)
+	fmt.Println(len(out), err) // 12 <nil>
+	bytespool.Put(out)
+}
+
+func poolDemo() {
+	data := bytes.Repeat([]byte("abc.123"), 1000)
+
+	dst, err := Zip(data)
+	fmt.Println("zip err:", err)      // zip err: <nil>
+	fmt.Println("zip len:", len(dst)) // zip len: 43
+
+	src, err := Unzip(dst)
+	fmt.Println("unzip err:", err)                     // unzip err: <nil>
+	fmt.Println("src == data", bytes.Equal(src, data)) // src == data true
+
+	// Suggestion (non-mandatory): After use, bytes can be put back to bytespool
+	bytespool.Put(dst)
+	bytespool.Put(src)
+
+	dst, err = ZipLevel(data, libdeflate.MaxCompressionLevel)
+	fmt.Println("zip err:", err)      // zip err: <nil>
+	fmt.Println("zip len:", len(dst)) // zip len: 43
+
+	src, err = Unzip(dst)
+	fmt.Println("unzip err:", err)                     // unzip err: <nil>
+	fmt.Println("src == data", bytes.Equal(src, data)) // src == data true
+
+	// Suggestion (non-mandatory): After use, bytes can be put back to bytespool
+	bytespool.Put(src)
+	bytespool.Put(dst)
+}
---
+++ examples/pool/zlib.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"sync"
+
+	"github.com/fufuok/go-libdeflate"
+)
+
+var (
+	zipPool   sync.Pool
+	unZipPool sync.Pool
+)
+
+func init() {
+	// Initialize compressor pool with default compression level
+	zipPool.New = func() interface{} {
+		c, err := libdeflate.NewCompressorAutoClose()
+		if err != nil {
+			panic(err)
+		}
+		return &c
+	}
+
+	// Initialize decompressor pool
+	unZipPool.New = func() interface{} {
+		dc, err := libdeflate.NewDecompressorAutoClose()
+		if err != nil {
+			panic(err)
+		}
+		return &dc
+	}
+}
+
+// Zip compresses data using DefaultCompressionLevel
+func Zip(data []byte) ([]byte, error) {
+	if len(data) == 0 {
+		return nil, nil
+	}
+
+	c := zipPool.Get().(*libdeflate.Compressor)
+	defer zipPool.Put(c)
+
+	_, dst, err := c.Compress(data, nil, libdeflate.ModeZlib)
+	if err != nil {
+		return nil, err
+	}
+	return dst, nil
+}
+
+// ZipLevel compresses data with specified compression level, without using object pool
+func ZipLevel(data []byte, level int) ([]byte, error) {
+	if len(data) == 0 {
+		return nil, nil
+	}
+
+	c, err := libdeflate.NewCompressorLevel(level)
+	if err != nil {
+		return nil, err
+	}
+	_, dst, err := c.Compress(data, nil, libdeflate.ModeZlib)
+	c.Close()
+	return dst, nil
+}
+
+// Unzip decompresses data
+func Unzip(data []byte) ([]byte, error) {
+	if len(data) == 0 {
+		return nil, nil
+	}
+
+	dc := unZipPool.Get().(*libdeflate.Decompressor)
+	defer unZipPool.Put(dc)
+
+	_, src, err := dc.Decompress(data, nil, libdeflate.ModeZlib)
+	return src, err
+}
